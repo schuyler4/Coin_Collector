7cbcd215924a3399800b27f4e47cdcf9adf1ade8
Marek Newton <marek.s.newton@gmail.com>
2016-07-05 11:21:31 -0600

    finished it

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 var canvas = document.getElementById("canvas")
 var ctx = canvas.getContext("2d")
+//images Varibles
+var goodCoin = new Image()
+goodCoin.src = 'goodCoin.png'
+
+var badCoin = new Image() 
+badCoin.src = 'badCoin.png'
+
+var playAgainBtn = new Image()
+playAgainBtn.src = 'playAgainBtn.png'
+
+var badMenu = new Image()
+badMenu.src = 'badMenu.png'
+
+var timeMenu = new Image()
+timeMenu.src = 'timeMenu.png'
+//sound Varibles
+var badSound = new Audio('badSound.wav')
+var coinSound = new Audio('pickUpSound.wav')
+
 //death Varibles
 deathByBomb = false
 ranOutOfTime = false
@@ -9,8 +28,9 @@ ctx.canvas.addEventListener('mousedown', function(event) {
 	var mouseX = event.clientX - ctx.canvas.offsetLeft
 	var mouseY = event.clientY - ctx.canvas.offsetTop 
 	if(!gameOver) {
-		if(mouseX > coin.x && coin.x + coin.width > mouseX
-			&& mouseY > coin.y && coin.y + coin.height > mouseY) {
+		if(coin.x < mouseX && coin.x + 42 > mouseX && coin.y < mouseY && coin.y + 42
+			> mouseY) {
+			coinSound.play()
 			score.count += 1
 			coin.x = Math.floor(Math.random() * 600)
 			coin.y = Math.floor(Math.random() * 400)
@@ -20,37 +40,38 @@ ctx.canvas.addEventListener('mousedown', function(event) {
 			bomb.y2 = Math.floor(Math.random() * 400)
 			bomb.x3 = Math.floor(Math.random() * 600)
 			bomb.y3 = Math.floor(Math.random() * 400)
-
-			}
-		else if(mouseX > bomb.x1 && bomb.x1 + bomb.width > mouseX
-			&& mouseY > bomb.y1  && bomb.y1 + bomb.height > mouseY) {
+		}
+		else if(bomb.x1 < mouseX && bomb.x1 + 42 > mouseX && bomb.y1 < mouseY && 
+			bomb.y1+ 42 > mouseY) {
+			badSound.play()
 			restartMenu.draw()
 			deathByBomb = true
 			gameOver = true
 		}
-		else if(mouseX > bomb.x2 && bomb.x2 + bomb.width > mouseX
-			&& mouseY > bomb.y2 && bomb.y2 + bomb.height > mouseY) {
+		else if(bomb.x2 < mouseX && bomb.x2 + 42 > mouseX && bomb.y2 < mouseY 
+			&& bomb.y2 + 42 > mouseY) {
+			badSound.play()
 			restartMenu.draw()
 			deathByBomb = true
 			gameOver = true
 		}
-		else if(mouseX > bomb.x3 && bomb.x3 + bomb.width > mouseX
-			&& mouseY > bomb.y3 && bomb.y3 + bomb.height > mouseY) {
+		else if(bomb.x3 < mouseX && bomb.x3 + 42 > mouseX && bomb.y3 < mouseY 
+			&& bomb.y3 + 42 > mouseY) {
+			badSound.play()
 			restartMenu.draw()
 			deathByBomb = true
 			gameOver = true
 		}
 	}
 	else {
-		if(mouseX > restartMenu.restartBtnX && restartMenu.
-			restartBtnX + 100 > mouseX && 
-			mouseY > restartMenu.restartBtnY && restartMenu.
-			restartBtnY + 50 > mouseY) {
+		if(restartMenu.restartBtnX < mouseX && restartMenu.restartBtnX + 100
+			> mouseX && restartMenu.restartBtnY < mouseY && restartMenu.restartBtnY + 
+			50 > restartMenu.restartBtnY) {
 			score.count = 0
 			timer.time = 1000
 			gameOver = false
-			coin.x = Math.floor(Math.random() * 600)
-			coin.y = Math.floor(Math.random() * 400)
+			coin.x = 0 //Math.floor(Math.random() * 600)
+			coin.y = 0//Math.floor(Math.random() * 400)
 			bomb.x1 = Math.floor(Math.random() * 600)
 			bomb.y1 = Math.floor(Math.random() * 400)
 			bomb.x2 = Math.floor(Math.random() * 600)
@@ -60,17 +81,13 @@ ctx.canvas.addEventListener('mousedown', function(event) {
 		}
 	}
 })
-
 var coin = {
-	width: 50,
-	height: 50,
-	x: Math.floor(Math.random() * 600),
-	y: Math.floor(Math.random() * 400),
+	x: 0, //Math.floor(Math.random() * 600),
+	y: 0 ,//Math.floor(Math.random() * 400),
+	size: 42,
 	draw: function() {
 		ctx.beginPath()
-		ctx.rect(this.x,this.y,this.width,this.height)
-		ctx.fillStyle = "yellow"
-		ctx.fill()
+		ctx.drawImage(goodCoin, this.x, this.y)
 		ctx.closePath()
 		if(this.x == bomb.x && this.y == bomb.y) {
 			coin.x = Math.floor(Math.random() * 600)
@@ -88,8 +105,6 @@ var coin = {
 }
 
 var bomb = {
-	width: 50,
-	height: 50,
 	x1: Math.floor(Math.random() * 600),
 	y1: Math.floor(Math.random() * 400),
 	x2: Math.floor(Math.random() * 600),
@@ -98,17 +113,15 @@ var bomb = {
 	y3: Math.floor(Math.random() * 400),
 	draw: function() {
 		ctx.beginPath()
-		ctx.rect(this.x1,this.y1,this.width,this.height)
-		ctx.rect(this.x2,this.y2,this.width,this.height)
-		ctx.rect(this.x3,this.y3,this.width,this.height)
-		ctx.fillStyle = "red"
-		ctx.fill()
+		ctx.drawImage(badCoin,this.x1,this.y1)
+		ctx.drawImage(badCoin,this.x2,this.y2)
+		ctx.drawImage(badCoin,this.x3,this.y3)
 		ctx.closePath()
 	},
 }
 
 var timer = {
-	time: 1000,
+	time: 999,
 	countDown: function() {
 		this.time -= 1
 		if(this.time == 0) {
@@ -119,8 +132,8 @@ var timer = {
 	draw: function() {
 		ctx.beginPath()
 		ctx.fillStyle = "white"
-		ctx.font = "30px Arail"
-		ctx.fillText("Time: " + this.time,300,30)
+		ctx.font = "30px Arial"
+		ctx.fillText("Time: " + this.time,460,30)
 		ctx.closePath()
 	}
 }
@@ -128,9 +141,9 @@ var score = {
 	count: 0,
 	draw: function() {
 		ctx.beginPath()
+		ctx.font = "30px Arial"
 		ctx.fillStyle = "white"
 		ctx.fill()
-		ctx.font = "30px Arial"
 		ctx.fillText("Score: " + this.count,10,30)
 		ctx.closePath()
 	}
@@ -140,33 +153,23 @@ var restartMenu = {
 	height:200,
 	x:200,
 	y:100,
-	restartBtnX: 250,
-	restartBtnY: 200,
+	restartBtnX: 250,//250
+	restartBtnY: 200,//200
 	draw: function() {
-		ctx.beginPath()
-		ctx.rect(this.x,this.y,this.width,this.height)
-		ctx.fillStyle = "blue"
-		ctx.fill()
-		ctx.closePath()
-		ctx.beginPath()
-		ctx.fillStyle = "white"
-		ctx.fill()
-		ctx.font = "30px Arial"
 		if(deathByBomb) {
-			ctx.fillText("you Blew UP",100,100)
+			ctx.drawImage(badMenu,this.x,this.y)
 		}
 		if(ranOutOfTime) {
-			ctx.fillText("you ran out of time",100,100)
+			ctx.drawImage(timeMenu,this.x,this.y)
 		}
-		ctx.fillText("your score was: " + score.count,100,140)
-		ctx.closePath()
-		ctx.beginPath()
-		ctx.rect(this.restartBtnX,this.restartBtnY,100,50)
-		ctx.fillStyle = "pink"
-		ctx.fill()
+		ctx.font = "15px Arial"
+		ctx.fillStyle = "white"
+		ctx.fillText(score.count,335,174)
+		ctx.drawImage(playAgainBtn,this.restartBtnX,this.restartBtnY)
 		ctx.closePath()
 	}
 }
+
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	score.draw()
